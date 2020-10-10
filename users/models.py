4d2e23a80ae7072a7546ca0becd91231e457f9f7
Betta-Lyon-Delsordo <lyondelsordo.betta@gmail.com>
2020-10-09 23:09:19 -0600

    Add custom user model and superuser

--- users/models.py
+++ users/models.py
@@ -2,5 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+LANGUAGE_CHOICES = [
+    ('ENGLISH', 'English'),
+    ('SPANISH', 'Spanish'),
+]
+
+
+
 class CustomUser(AbstractUser):
-    language = models.CharField(max_length=20, null=True, blank=True)
+    language = models.CharField(max_length=7, choices=LANGUAGE_CHOICES)
+    
