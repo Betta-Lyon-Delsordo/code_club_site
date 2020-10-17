c8a66499d48827ab214bbb74c535bcc644eb84a7
Betta-Lyon-Delsordo <lyondelsordo.betta@gmail.com>
2020-10-17 00:32:50 -0600

    Add new Post views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'body',)
+    template_name = 'post_edit.html'
+    
+class PostDetailView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
