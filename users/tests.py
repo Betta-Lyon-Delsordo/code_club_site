3781345b3ca259601fb189bb9bd8dd3fa6dc1458
Betta-Lyon-Delsordo <lyondelsordo.betta@gmail.com>
2020-11-04 00:53:23 -0700

    Add testing to pages and users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import CustomUser
+
+class UsersTests(TestCase):
+    
+  def setUp(self):
+    self.user = get_user_model().objects.create_user(
+      username='testuser',
+      email='test@email.com',
+      password='secret'
+      
+    )
+    
+    
+
+  def test_sign_up_view(self):
+    response = self.client.get(reverse('home'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'home.html')
+  
+  def test_user(self):
+    self.assertEqual(self.user.username, 'testuser')
+    self.assertEqual(self.user.email, 'test@email.com')
+    
+    
+    
+
